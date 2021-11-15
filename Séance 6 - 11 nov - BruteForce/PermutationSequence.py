2a126331e32c0970a3af91072adc8f885148e785
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2021-11-15 20:58:44 +0100

    Add files via upload

---
+++ Séance 6 - 11 nov - BruteForce/PermutationSequence.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        S=""
+        for i in range(1,n+1):
+            S+=str(i)
+        V=permutations(S,n)
+        i=0
+        for v in V:
+            if i==k-1:
+                return ''.join(v)
+            i+=1
+        
