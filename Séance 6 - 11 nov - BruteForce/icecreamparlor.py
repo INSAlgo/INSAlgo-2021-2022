4ce6fd2e23ca62c8b02aaddda457ab4c80114d48
SebGoll <50230837+SebGoll@users.noreply.github.com>
2021-11-21 23:12:32 +0100

    Delete Séance 6 - 11 nov - BruteForce directory

--- Séance 6 - 11 nov - BruteForce/icecreamparlor.py
+++
@@ -1,9 +0,0 @@
-
-def icecreamParlor(m, arr):
-    for x in range(len(arr)):
-        n=m-arr[x]
-        for y in range(len(arr)):
-            o=n-arr[y]
-            if(o==0) and x!=y:
-                return (x+1,y+1)
-    
