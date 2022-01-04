ed0e2bbfe8e4b25d15eb60073e17ed26cabda0c8
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2022-01-04 18:24:37 +0100

    Delete Séance 2 - 12 oct - Complexité Algorithmique directory

--- Séance 2 - 12 oct - Complexité Algorithmique/flatland-space-stations.py
+++
@@ -1,8 +0,0 @@
-def flatlandSpaceStations(n, c):
-    c.sort()
-    maxi=c[0]
-    
-    for i in range(len(c)-1):
-        maxi = max(maxi,int((c[i+1]-c[i])/2))
-    maxi = max(maxi,n-1-c[-1])
-    return maxi
