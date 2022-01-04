a49a1b23cb4dc0d884ac00ce8baf72a4db8118b5
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2022-01-04 18:25:42 +0100

    Delete Séance 3 - 19 oct - Algorithmes récursifs directory

--- Séance 3 - 19 oct - Algorithmes récursifs/fibonacci-number.py
+++
@@ -1,9 +0,0 @@
-def fib(self, n: int) -> int:
-        
-        def F(i):
-            
-            if i==0:return 0
-            if i==1:return 1
-            return F(i-1)+F(i-2)
-        
-        return F(n)
