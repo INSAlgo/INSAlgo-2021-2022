acf6138d24234d95632b471cb5c1cf340c5be23b
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2022-01-04 18:33:08 +0100

    Delete Séance 8 - 30 nov - Greedy directory

--- Séance 8 - 30 nov - Greedy/chief-hopper.py
+++
@@ -1,8 +0,0 @@
-#Here the strategy is to determine the minimal cost at the end of the course
-# and to determine step by step the minimal cost before each building. 
-def chiefHopper(arr):
-    enNecc=[0]
-    for lastelem in arr[::-1]:
-        enNecc.append(math.ceil((lastelem+enNecc[-1])/2))
-    print(enNecc)
-    return(enNecc[-1])
